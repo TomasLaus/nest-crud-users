c30e06532338efea50285fc59788837cde3b18b6
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2024-02-09 11:02:01 -0300

    Auth Dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,12 +13,12 @@ export class AuthController {
 
   //Post /auth/signup
   @Post('signup')
-  signup() {
-    return this.authService.signup()
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup();
   }
 
   @Post('signin')
   signin() {
-    return this.authService.singin()
+    return this.authService.singin();
   }
 }
