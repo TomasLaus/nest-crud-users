dcd1f61a7079bf5c950c3c4dbeea4434f92c5157
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2024-02-13 19:30:06 -0300

    Add user authentication protection with Nest JS Guards

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+  
+  // GET /users/me
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() req: Request) {
+    return req.user;
+  }
+}
