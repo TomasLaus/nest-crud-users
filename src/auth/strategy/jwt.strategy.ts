b3639933ad032519cc0cc10f5cf7924b77db17f3
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2024-02-14 10:41:04 -0300

    Testing - DotEnv CLI
    Add Basic testing logic, connected to Docker PSQL DB, and a .env testing file. Also fix some imports that were not working

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -5,7 +5,7 @@ import {
   Strategy,
   ExtractJwt,
 } from 'passport-jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service'; 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
-import { JwtGuard } from 'src/auth/guard';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  
   // GET /users/me
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
